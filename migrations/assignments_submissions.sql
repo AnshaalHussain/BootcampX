928d599bd250490d73abda9609a902f029b0e9ee
Anshaal Hussain <anshaalhussain@gmail.com>
2021-10-12 08:20:40 -0400

    data join tables

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -16,3 +16,8 @@ CREATE TABLE assignment_submissions (
   duration INTEGER,
   submission_date DATE
 );
+
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.name = 'Ibrahim Schimmel';
